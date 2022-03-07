dc4fde71318e78c6a7e784b06b1b07fea4618a5d
RaisFardeev <raisfardeev5@gmail.com>
2022-03-07 15:42:36 +0300

    Initial commit

--- src/code.cpp
+++ src/code.cpp
@@ -7,56 +7,93 @@ namespace assignment {
 
   // Task 1
   int squared_diff(int left, int right) {
-
-    // Write your code here ...
-
-    return 0;
+    return (left - right) * (left - right);
   }
-
   // Task 2
   bool check_bit(int mask, int bit_pos) {
-
-    // Write your code here ...
-
-    return false;
+    int bin = 0;
+    int k = 1;
+    while (mask){
+      bin += (mask%2)*k;
+      k *= 10;
+      mask /= 2;
+    }
+    int y = 1;
+    for (int i = 1;i<bit_pos+1;i++){
+      y *= 10;
+    }
+    bin = bin / y;
+    bin %= 10;
+    if (bin == 1) {
+      return true;
+    }else{
+      return false;
+    }
   }
 
   // Task 3
   int max3(int left, int middle, int right) {
-
-    // Write your code here ...
-
-    return 0;
+    int max = 0;
+    if (left>=middle and left>=right){
+      max = left;
+    }if(middle>=left and middle >= right){
+      max = middle;
+    }if(right >= left and right >= middle){
+      max = right;
+    }
+    return max;
   }
 
   // Task 4
   void swap_args(int *left, int *right) {
-
-    // Write your code here ...
+    int tmp;
+    tmp = *left;
+    *left = *right;
+    *right = tmp;
   }
 
   // Task 5
   int arr_sum(int *arr, int length) {
-
-    // Write your code here ...
-
-    return 0;
+    int sum = 0;
+    for (int i=0;i<length;i++) {
+      sum += arr[i];
+    }
+    return sum;
   }
 
   // Task 6
   int *find_max_elem(int *arr, int length) {
-
-    // Write your code here ...
-
-    return nullptr;
+    int max = arr[0];
+    int* maxx;
+    for(int i = 1;i<length;i++){
+      if (arr[i] > max){
+        max = arr[i];
+      }
+    }
+    for (int i = 0;i<length;i++){
+      if (max == arr[i]){
+        maxx = &arr[i];
+      }
+    }
+    if (*arr == false or length<=0) {
+      return nullptr;
+    }else{
+      return maxx;
+    }
   }
 
   // Task 7
   int *allocate_arr(int length, int init_value) {
-
-    // Write your code here ...
-
-    return nullptr;
+    const int a = length;
+    int b = init_value;
+    if (a<=0){
+      return nullptr;
+    }
+    int *r = new int[a];
+    for (int i = 0;i<a;i++){
+      r[i] = b;
+    }
+    return r;
   }
 
   // Task 8
@@ -72,5 +109,5 @@ namespace assignment {
 
     // Write your code here ...
   }
-
 }  // namespace assignment
+
